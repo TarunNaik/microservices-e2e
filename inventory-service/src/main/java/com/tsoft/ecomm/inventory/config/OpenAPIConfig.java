4457a0d7b8fa8cddd8960e5ccfd4c46ff26cff39
tarunnaik <tarun.naik@gmail.com>
2025-01-27 11:29:33 +0530

    Inventory Service changes for adding Open API documentation

---
+++ inventory-service/src/main/java/com/tsoft/ecomm/inventory/config/OpenAPIConfig.java
@@ -0,0 +1,18 @@
+package com.tsoft.ecomm.inventory.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info()
+                .title("Inventory Service API")
+                .version("1.0")
+                .description("Documentation Inventory API v1.0")
+                .license(new io.swagger.v3.oas.models.info.License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+}
