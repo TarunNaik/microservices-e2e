a83e6d6dc063490fbdc59d512284492a35d59ace
tarunnaik <tarun.naik@gmail.com>
2025-01-27 12:30:39 +0530

    Api Doc with Open API Configured for Gateway Project

--- api-gateway/src/main/java/com/tsoft/ecomm/gateway/route/CustomRouter.java
+++ api-gateway/src/main/java/com/tsoft/ecomm/gateway/route/CustomRouter.java
@@ -5,10 +5,11 @@ import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctio
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.function.RequestPredicate;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
 
 @Configuration
 public class CustomRouter {
@@ -17,16 +18,24 @@ public class CustomRouter {
     @Value("${product.service.url}")
     private String productServiceUrl;
 
-    @Value("${product.service.url}")
+    @Value("${order.service.url}")
     private String orderServiceUrl;
 
-    @Value("${product.service.url}")
+    @Value("${inventory.service.url}")
     private String inventoryServiceUrl;
 
     @Bean
     public RouterFunction<ServerResponse> productServiceRoute() {
         return GatewayRouterFunctions.route("product-service")
                 .route(RequestPredicates.path("/v1/api/product/**"), HandlerFunctions.http(productServiceUrl))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> productServiceApiDocRoute() {
+        return GatewayRouterFunctions.route("product-service-api-doc")
+                .route(RequestPredicates.path("/aggregate/product-service/v3/api-docs"), HandlerFunctions.http(productServiceUrl))
+                .filter(setPath("/v3/api-docs"))
                 .build();
     }
 
@@ -38,11 +47,26 @@ public class CustomRouter {
     }
 
     @Bean
+    public RouterFunction<ServerResponse> orderServiceApiDocRoute() {
+        return GatewayRouterFunctions.route("order-service-api-doc")
+                .route(RequestPredicates.path("/aggregate/order-service/v3/api-docs"), HandlerFunctions.http(orderServiceUrl))
+                .filter(setPath("/v3/api-docs"))
+                .build();
+    }
+
+    @Bean
     public RouterFunction<ServerResponse> inventoryServiceRoute() {
         return GatewayRouterFunctions.route("inventory-service")
                 .route(RequestPredicates.path("/v1/api/inventory/**"), HandlerFunctions.http(inventoryServiceUrl))
                 .build();
     }
 
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceApiDocRoute() {
+        return GatewayRouterFunctions.route("inventory-service-api-doc")
+                .route(RequestPredicates.path("/aggregate/inventory-service/v3/api-docs"), HandlerFunctions.http(inventoryServiceUrl))
+                .filter(setPath("/v3/api-docs"))
+                .build();
+    }
 
 }
