070d18bb5c87bdf72e21ea25cd9e5aefbd28c4e9
tarunnaik <tarun.naik@gmail.com>
2025-01-29 20:02:06 +0530

    Updated for Security Config using Keycloak

--- api-gateway/src/main/java/com/tsoft/ecomm/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/tsoft/ecomm/gateway/config/SecurityConfig.java
@@ -1,2 +1,23 @@
-package com.tsoft.ecomm.gateway.config;public class SecurityConfig {
+package com.tsoft.ecomm.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    private String[] whitelistUrls = new String[] {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui/**",
+            "/swagger-resources/**", "/api-docs/**", "/aggregate/**", "/actuator/prometheus"};
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(whitelistUrls).permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
 }
