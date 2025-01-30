b070bdc0ecd7e8918a28887c9a341f6d67e0ab90
tarunnaik <tarun.naik@gmail.com>
2025-01-30 10:42:44 +0530

    Elastic indexer project added

---
+++ elastic-indexer/src/main/java/com/tsoft/ecomm/indexer/ElasticIndexerApplication.java
@@ -0,0 +1,13 @@
+package com.tsoft.ecomm.indexer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticIndexerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticIndexerApplication.class, args);
+	}
+
+}
---
+++ elastic-indexer/src/test/java/com/tsoft/ecomm/indexer/ElasticIndexerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsoft.ecomm.indexer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticIndexerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
